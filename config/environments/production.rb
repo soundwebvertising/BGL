ca46e986cd092e75b6390fe409f95f04653c10a0
Bobbi <soundwebvertising@gmail.com>
2020-10-14 13:36:19 -0700

    added production values for sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  #below two lines added for production deployment to heroku 10/14/2020
-  config.action_mailer.default_url_options = { host: 'https://bobbisguitarlessons.herokuapp.com/' }
-  #https://bobbisguitarlessons.herokuapp.com/
+  #below four lines added for production deployment to heroku 10/14/2020
+  config.action_mailer.default_url_options = { :host => 'https://bobbisguitarlessons.herokuapp.com', protocol => 'https' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
 
   # Code is not reloaded between requests.
   config.cache_classes = true
