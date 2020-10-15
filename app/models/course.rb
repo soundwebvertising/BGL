3f47f6e6d7ebb4fb6badc79fa12c4efc9d0db0f8
Bobbi <soundwebvertising@gmail.com>
2020-10-15 13:35:25 -0700

    addded courses search funtionality using ransack

--- app/models/course.rb
+++ app/models/course.rb
@@ -11,13 +11,13 @@ class Course < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
 
-  # friendly_id :generated_slug, use: :slugged
-  # def generated_slug 
-  #   require 'securerandom'
-  #   @random_slug ||= persisted? ? friendly_id : SecureRandom.hex(4)
-  # end
+  LANGUAGES = [:"English", :"Spanish", :"German", :"Russian"]
+  def self.languages
+    LANGUAGES.map { |language| [language, language] }
+  end
 
-  # def to_s
-  #   slug 
-  # end
+  LEVELS = [:"Beginner", :"Intermediate", :"Advanced"]
+  def self.levels
+    LEVELS.map { |level| [level, level] }
+  end
 end
