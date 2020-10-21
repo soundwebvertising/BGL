c083a322cad988ca14026367847c39d06efafef4
Bobbi <soundwebvertising@gmail.com>
2020-10-21 08:37:40 -0700

    added validation to roles - lesson 49

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -11,8 +11,10 @@ class CoursesController < ApplicationController
     #   #@q = Course.ransack(params[:q])
     #   #@courses = @q.result.includes(:user)
     # end
+    
     @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search) #navbar search
     @courses = @ransack_courses.result.includes(:user)
+   
   end
   
   # GET /courses/1
