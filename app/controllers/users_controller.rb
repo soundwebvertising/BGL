cf024addaef0067ff63c0ceac5d2fc8c226d4bfc
Bobbi <soundwebvertising@gmail.com>
2020-10-21 07:57:20 -0700

    added edit to user roles - lesson 48

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,36 @@
 class UsersController < ApplicationController 
-   def index
-     #@users = User.all.order(created_at: :desc)
-     @q = User.ransack(params[:q])
-     @users = @q.result(distinct: true)
-   end
+  before_action :set_user, only: [:edit, :update]
+
+  def index
+    #@users = User.all.order(created_at: :desc)
+    @q = User.ransack(params[:q])
+    @users = @q.result(distinct: true)
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @users_path, notice: "User was successfully updared."
+    else
+      render :edit
+    end
+  end
+
+  def show
+  
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit({role_ids: []})
+  end
+
 end
